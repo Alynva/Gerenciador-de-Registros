0ae2d406ee12e54c21abcfed94b85617b467014c
Alynva <alynva@gmail.com>
2017-10-30 01:27:06 -0200

    FIZ COISA PRA KRL
    Adicionado a busca e a remoção, porém na remoção falta registrar no cabeçalho a quantidade de registros já removidos e diminuir os removidos do total de registros

--- main.cpp
+++ main.cpp
@@ -49,7 +49,8 @@ int main() {
 				"\t-1 - Sair\n" \
 				"\t 0 - Zerar arquivo\n" \
 				"\t 1 - Inserir novo registro\n" \
-				"\t 2 - Busca por registro");
+				"\t 2 - Busca por registro\n" \
+				"\t 3 - Remover registro");
 
 		printf("\nDigite o opção: ");
 		scanf("%d", &opcao);
@@ -101,8 +102,6 @@ int main() {
 					cout << "Numero do bloco:" << endl << buffer_index << endl << endl;
 					cout << "Bloco:" << endl << buffer << endl << endl;
 
-					++n_registros;
-
 					string chave = "";
 					while (strlen(chave.c_str()) != TAM_CHAVE) { // Pede a chave do registro enquanto a inserida não tiver um comprimento de 3 caracteres
 						cout << "Insira a chave para o registro (XXX): ";
@@ -136,17 +135,17 @@ int main() {
 					nome.append(TAM_NOME - strlen(nome.c_str()), ' '); // Preenche o restante com espaços
 
 					cout << "\nDados reconhecidos:" << endl;
-					cout << "Chave: " << chave << endl;
-					cout << "Telefone: (" << numero_ddd << ") " << numero_prefixo << "-" << numero_sufixo << endl;
-					cout << "Data de nascimento: " << data_dia << "/" << data_mes << "/" << data_ano << endl;
-					cout << "E-mail: " << email << endl;
-					cout << "Nome: " << nome << endl << endl;
+					cout << "Chave:\t\t\t" << chave << endl;
+					cout << "Telefone:\t\t(" << numero_ddd << ") " << numero_prefixo << "-" << numero_sufixo << endl;
+					cout << "Data de nascimento:\t" << data_dia << "/" << data_mes << "/" << data_ano << endl;
+					cout << "E-mail:\t\t\t" << email << endl;
+					cout << "Nome:\t\t\t" << nome << endl << endl;
 
 					pause("gravar o registro"); // Confirma a ação com o usuário
 
 					// Grava os dados
 					if (n_registros < 4) {
-						
+						cout <<  (TAM_CABECALHO + n_registros * TAM_REGISTRO) << endl;
 						snprintf(
 							buffer + (TAM_CABECALHO + n_registros * TAM_REGISTRO),
 							TAM_CHAVE+1,
@@ -183,15 +182,15 @@ int main() {
 						);
 
 					} else {
-						
+
 						snprintf(
-							buffer + (n_registros % 5) * TAM_REGISTRO,
+							buffer + ((n_registros + 1) % 5) * TAM_REGISTRO,
 							TAM_CHAVE+1,
 							"%s",
 							chave.c_str()
 						);
 						snprintf(
-							buffer + (n_registros % 5) * TAM_REGISTRO + TAM_CHAVE,
+							buffer + ((n_registros + 1) % 5) * TAM_REGISTRO + TAM_CHAVE,
 							(TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO)+1,
 							"%s%s%s",
 							numero_ddd.c_str(),
@@ -199,7 +198,7 @@ int main() {
 							numero_sufixo.c_str()
 						);
 						snprintf(
-							buffer + (n_registros % 5) * TAM_REGISTRO + TAM_CHAVE + (TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO),
+							buffer + ((n_registros + 1) % 5) * TAM_REGISTRO + TAM_CHAVE + (TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO),
 							(TAM_DATA_DIA + TAM_DATA_MES + TAM_DATA_ANO)+1,
 							"%s%s%s",
 							data_dia.c_str(),
@@ -207,13 +206,13 @@ int main() {
 							data_ano.c_str()
 						);
 						snprintf(
-							buffer + (n_registros % 5) * TAM_REGISTRO + TAM_CHAVE + (TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO) + (TAM_DATA_DIA + TAM_DATA_MES + TAM_DATA_ANO),
+							buffer + ((n_registros + 1) % 5) * TAM_REGISTRO + TAM_CHAVE + (TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO) + (TAM_DATA_DIA + TAM_DATA_MES + TAM_DATA_ANO),
 							TAM_EMAIL+1,
 							"%s",
 							email.c_str()
 						);
 						snprintf(
-							buffer + (n_registros % 5) * TAM_REGISTRO + TAM_CHAVE + (TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO) + (TAM_DATA_DIA + TAM_DATA_MES + TAM_DATA_ANO) + TAM_EMAIL,
+							buffer + ((n_registros + 1) % 5) * TAM_REGISTRO + TAM_CHAVE + (TAM_NUMERO_DDD + TAM_NUMERO_PREFIXO + TAM_NUMERO_SUFIXO) + (TAM_DATA_DIA + TAM_DATA_MES + TAM_DATA_ANO) + TAM_EMAIL,
 							TAM_NOME+1,
 							"%s",
 							nome.c_str()
@@ -221,9 +220,8 @@ int main() {
 
 					}
 
-					cout << "Numero de registros: " << n_registros << endl << endl;
-					cout << "Numero do bloco:" << endl << buffer_index << endl << endl;
-					cout << "Bloco:" << endl << buffer << endl << endl;
+					++n_registros;
+
 					fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
 					fwrite(buffer, sizeof(char), sizeof(buffer), lf);
 
@@ -232,7 +230,7 @@ int main() {
 					rewind(lf);
 					cabecalho = "Numero de registros: ";
 					cabecalho.append(to_string(n_registros));
-					cabecalho.append(TAM_REGISTRO - strlen(cabecalho.c_str()), ' ');
+					cabecalho.append(TAM_CABECALHO - strlen(cabecalho.c_str()), ' ');
 					fprintf(lf, "%s", cabecalho.c_str());
 
 					fflush(lf); // Força a atualização do arquivo
@@ -243,12 +241,206 @@ int main() {
 				{
 					string chave = "";
 					do {
-						cout << "Insira a chave para ser buscada: ";
+						cout << "Insira a chave para ser buscada (XXX): ";
 						cin >> chave;
 					} while (strlen(chave.c_str()) != 3);
 
+					lf = freopen("arquivo.bin", "r+b", lf);
+					
+					if(lf == NULL) { // Verifica erros
+						printf("Erro na abertura do arquivo.\n");
+						return 2;
+					}
 
+					int buffer_index = -1;
+					bool achou = false;
+					char test_chave[3] = "";
+							fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
+							fread(buffer, sizeof(char), sizeof(buffer), lf);
+
+					string found_chave = "",
+						found_numero_ddd = "",
+						found_numero_prefixo = "",
+						found_numero_sufixo = "",
+						found_data_dia = "",
+						found_data_mes = "",
+						found_data_ano = "",
+						found_email = "",
+						found_nome = "";
+
+					do {
+						buffer_index++;
+						fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
+						fread(buffer, sizeof(char), sizeof(buffer), lf);
+
+						int seek_buffer = 0;
+						int max_registros = 5;
+						if (buffer_index == 0) {
+							seek_buffer = TAM_CABECALHO;
+							max_registros = 4;
+						}
+
+						if (n_registros / 5 + 1 == 1) {
+							max_registros = n_registros;
+						} else if (buffer_index < n_registros / 5 - 1) {
+							max_registros = 5;
+						} else {
+							max_registros = (n_registros % 5) + 1;
+						}
+
+						for (int i = 0, registro_index = 0; i < max_registros; i++, registro_index++) {
+							fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
+							fread(buffer, sizeof(char), sizeof(buffer), lf);
+
+							string pattern = "%*";
+							pattern.append(to_string(seek_buffer + registro_index * TAM_REGISTRO));
+							pattern.append("[^\n]%");
+							pattern.append(to_string(TAM_CHAVE));
+							pattern.append("[^\n]");
+
+							sscanf(buffer, pattern.c_str(), test_chave);
+
+							if (!strcmp(test_chave, chave.c_str())) {
+								int j = seek_buffer + registro_index * TAM_REGISTRO;
+								for (int k = 0; k < TAM_CHAVE; j++, k++) {
+									found_chave.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_NUMERO_DDD; j++, k++) {
+									found_numero_ddd.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_NUMERO_PREFIXO; j++, k++) {
+									found_numero_prefixo.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_NUMERO_SUFIXO; j++, k++) {
+									found_numero_sufixo.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_DATA_DIA; j++, k++) {
+									found_data_dia.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_DATA_MES; j++, k++) {
+									found_data_mes.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_DATA_ANO; j++, k++) {
+									found_data_ano.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_EMAIL; j++, k++) {
+									found_email.append(to_string(buffer[j]));
+								}
+								for (int k = 0; k < TAM_NOME; j++, k++) {
+									found_nome.append(to_string(buffer[j]));
+								}
+
+								achou = true;
+								break;
+							}
+						}
+
+					} while (!achou && buffer_index < n_registros / 5);
+
+					cout << endl;
+
+					if (!achou) {
+						cout << "Não foi encontrado nenhum registro com esta chave." << endl;
+					} else {
+						cout << "A chave foi encontrada! Aqui estão os dados:" << endl << endl;
+						cout << "Chave:\t\t\t" << found_chave << endl;
+						cout << "Telefone:\t\t(" << found_numero_ddd << ") " << found_numero_prefixo << "-" << found_numero_sufixo << endl;
+						cout << "Data de nascimento:\t" << found_data_dia << "/" << found_data_mes << "/" << found_data_ano << endl;
+						cout << "E-mail:\t\t\t" << found_email << endl;
+						cout << "Nome:\t\t\t" << found_nome << endl;
+					}
+
+					cout << endl;
+
+					pause();
 				}
+				break;
+
+			case 3:
+				{
+					string chave = "";
+					do {
+						cout << "Insira a chave para ser excluída (XXX): ";
+						cin >> chave;
+					} while (strlen(chave.c_str()) != 3);
+
+					lf = freopen("arquivo.bin", "r+b", lf);
+					
+					if(lf == NULL) { // Verifica erros
+						printf("Erro na abertura do arquivo.\n");
+						return 2;
+					}
+
+					int buffer_index = -1,
+						seek_buffer = 0,
+						registro_index = 0;
+					bool achou = false;
+					char test_chave[3] = "";
+
+					do {
+						buffer_index++;
+
+						seek_buffer = 0;
+						int max_registros = 5;
+						if (buffer_index == 0) {
+							seek_buffer = TAM_CABECALHO;
+							max_registros = 4;
+						}
+
+						if (n_registros / 5 + 1 == 1) {
+							max_registros = n_registros;
+						} else if (buffer_index < n_registros / 5 - 1) {
+							max_registros = 5;
+						} else {
+							max_registros = (n_registros % 5) + 1;
+						}
+
+						registro_index = 0;
+						for (int i = 0; i < max_registros; i++, registro_index++) {
+							fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
+							fread(buffer, sizeof(char), sizeof(buffer), lf);
+
+							string pattern = "%*";
+							pattern.append(to_string(seek_buffer + registro_index * TAM_REGISTRO));
+							pattern.append("[^\n]%");
+							pattern.append(to_string(TAM_CHAVE));
+							pattern.append("[^\n]");
+
+							sscanf(buffer, pattern.c_str(), test_chave);
+
+							if (!strcmp(test_chave, chave.c_str())) {
+								achou = true;
+								break;
+							}
+						}
+
+					} while (!achou && buffer_index < n_registros / 5);
+
+					cout << endl;
+
+					if (!achou) {
+						cout << "Não foi encontrado nenhum registro com esta chave." << endl;
+					} else {
+						
+						fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
+						fread(buffer, sizeof(char), sizeof(buffer), lf);
+
+						int j = seek_buffer + registro_index * TAM_REGISTRO;
+						for (int k = 0; k < TAM_CHAVE; j++, k++) {
+							buffer[j] = '*';
+						}
+						
+						fseek(lf, buffer_index * TAM_BLOCK, SEEK_SET);
+						fwrite(buffer, sizeof(char), sizeof(buffer), lf);
+
+						fflush(lf);
+						
+						cout << "O registro foi excluído com sucesso." << endl;
+					}
+
+					pause();
+				}
+				break;
 
 			default:
 				cout << "Opção inválida." << endl;
